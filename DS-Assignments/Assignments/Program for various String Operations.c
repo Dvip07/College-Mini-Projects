f23d16091a2123fb7e30dc68e8ddf7a6f14456d4
Dvip <64122205+Dvip07@users.noreply.github.com>
2021-12-06 08:52:22 +0530

    Update Program for various String Operations.c

--- DS-Assignments/Assignments/Program for various String Operations.c
+++ DS-Assignments/Assignments/Program for various String Operations.c
@@ -6,11 +6,6 @@ int main()
     char nickname[20];
 
     printf("Enter your Nick name:");
-
-    /* I am reading the input string and storing it in nickname
-     * Array name alone works as a base address of array so
-     * we can use nickname instead of &nickname here
-     */
     scanf("%s", nickname);
 
     /*Displaying String*/
